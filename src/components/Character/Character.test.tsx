5760956860a3a5baaac70c44961f02f20c48dfa8
ZipengChen1832 <zipengchen1832@gmail.com>
2024-05-06 01:29:43 -0400

    tests

--- src/components/Character/Character.test.tsx
+++ src/components/Character/Character.test.tsx
@@ -1,20 +1,52 @@
-import { describe, expect, test } from "vitest";
+import { afterEach, describe, expect, test } from "vitest";
 import Character from "./Character";
 import { CharacterType } from "../../types/characterType";
-import { render } from "@testing-library/react";
+import { render, cleanup } from "@testing-library/react";
 
 describe("<Character />", () => {
+  afterEach(() => {
+    cleanup();
+  });
   test('shows fields for "name" and "culture"', async () => {
     const character = {
       name: "Jon",
       culture: "Northmen",
       books: ["https://www.anapioficeandfire.com/api/books/5"],
     } as CharacterType;
-    const { getByText, debug } = render(<Character character={character} />);
+    const { getByText } = render(<Character character={character} />);
     expect(getByText("name")).toBeInTheDocument();
-    debug();
   });
-  test("shows culture if it is present", async () => {});
-  test("shows alias if no name is present", async () => {});
-  test("shows how many books this characters made an appearance in", async () => {});
+  test("shows culture if it is present", async () => {
+    const character = {
+      name: "Jon",
+      culture: "Northmen",
+      books: ["https://www.anapioficeandfire.com/api/books/5"],
+    } as CharacterType;
+    const { getByText } = render(<Character character={character} />);
+    expect(getByText("culture")).toBeInTheDocument();
+    expect(getByText("Northmen")).toBeInTheDocument();
+  });
+  test("shows alias if no name is present", async () => {
+    const character = {
+      aliases: ["Jon Snow"],
+      culture: "Northmen",
+      books: ["https://www.anapioficeandfire.com/api/books/5"],
+    } as CharacterType;
+    const { getByText } = render(<Character character={character} />);
+    expect(getByText("name")).toBeInTheDocument();
+    expect(getByText("Jon Snow")).toBeInTheDocument();
+  });
+  test("shows how many books this characters made an appearance in", async () => {
+    const character = {
+      name: "Jon",
+      culture: "Northmen",
+      books: [
+        "https://www.anapioficeandfire.com/api/books/5",
+        "https://www.anapioficeandfire.com/api/books/8",
+      ],
+    } as CharacterType;
+    const { getByText } = render(<Character character={character} />);
+    expect(getByText("Number of Books:")).toBeInTheDocument();
+    expect(getByText("2")).toBeInTheDocument();
+  });
 });
